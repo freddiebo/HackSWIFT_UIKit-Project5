f5f2df32afeb04fdec257edf0a724d551c234ef8
VBondarev <freddiebo@yandex.ru>
2019-12-09 18:01:23 +0300

    Initial Commit

---
+++ Project5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by  Vladislav Bondarev on 09.12.2019.
+//  Copyright © 2019 Vladislav Bondarev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
